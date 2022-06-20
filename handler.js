2bc8fdf6731a0bda9838e5df9a81f29c4c401a6e
abhin1509 <abhin1509@gmail.com>
2022-06-20 18:35:01 +0530

    implemented resize image

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 const crypto = require("crypto");
+const resizeImg = require('resize-img');
+const fetch = require('node-fetch');
 
 const BUCKET_NAME = process.env.BUCKET_NAME;
 
@@ -21,7 +23,7 @@ const getId = () => {
   return crypto.randomBytes(8).toString('hex');
 }
 
-const uploadToBucket = (contentType, ext, isPublic) => {
+const uploadToBucket = async (contentType, ext, isPublic) => {
   let filename = getId(); // random file name
   var s3Params = {
     Bucket: BUCKET_NAME,
@@ -36,15 +38,49 @@ const uploadToBucket = (contentType, ext, isPublic) => {
   return sendResponse(200, signedUrl, imgUrl);
 }
 
+const resize = async (imageURL, height, width) => {
+  const temp = await fetch(imageURL);
+  const blob = await temp.buffer();
+  const w = parseInt(width);
+  const h = parseInt(height);
+  const image = await resizeImg(blob, { width: w, height: h });
+
+  let imgName = imageURL.split(".com/")[1];
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: imgName,
+    Body: image,
+    ACL: 'public-read',
+    ContentType: "image/jpeg"
+  };
+  const res = await S3.putObject(params).promise();
+  console.log("uploadResult :: " + JSON.stringify(res));
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      success: true,
+      message: "uploaded successfully",
+    })
+  }
+}
+
 module.exports.hello = async (event) => {
   console.log(event);
   try {
     if (event.httpMethod === 'POST' && event.path === '/') {
       const { contentType, ext, isPublic } = JSON.parse(event.body);
       console.log(contentType, ext, isPublic);
-      return uploadToBucket(contentType, ext, isPublic);
+      return await uploadToBucket(contentType, ext, isPublic);
     }
-    
+    if (event.httpMethod === 'POST' && event.path === '/resize/') {
+      const { imgUrl, height, width } = JSON.parse(event.body);
+      console.log(imgUrl, height, width);
+      return await resize(imgUrl, height, width);
+    }
+
   }
   catch (error) {
     console.log(error);
